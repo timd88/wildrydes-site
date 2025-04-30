030067874d8f233eda8264c76d9bd7fad20d8d40
timd88 <timdcn@gmail.com>
2025-04-30 16:39:26 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-east-1_acarhdMpO', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '17dgdfh17naf4hcu7l8c8pui67', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_fNwBT1iWD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5pn5l6ql6q02c0f2ddovvp0igc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
