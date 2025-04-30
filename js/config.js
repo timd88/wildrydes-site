938257353ee84ebe565af213bc08727e04b02008
timd88 <timdcn@gmail.com>
2025-04-30 16:43:56 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_fNwBT1iWD', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5pn5l6ql6q02c0f2ddovvp0igc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientsecret: 'c43gtuc54ghp3bu1gieitgf038q113btmqli51e7m26ev962iup',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
